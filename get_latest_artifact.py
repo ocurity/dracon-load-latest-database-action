9ccc2d9a58d014a0a55ff8114db5bded59d63486
spyros <morfeas3000@gmail.com>
2022-04-22 19:00:31 +0100

    wip

--- get_latest_artifact.py
+++ get_latest_artifact.py
@@ -1,3 +1,4 @@
+from urllib import response
 import requests
 from dateutil import parser
 import os
@@ -25,10 +26,15 @@ resp = requests.get(artifact_url, stream=True,headers=header)
 with open("/tmp/latest.zip", "wb") as fl:
     for chunk in resp.iter_content():
         fl.write(chunk)
+try:
+    with zipfile.ZipFile("/tmp/latest.zip") as zip:
+        print("Extracting files to %s"%os.environ["INPUT_OUTPUT_DIR"])
+        zip.extractall(os.environ["INPUT_OUTPUT_DIR"])
 
-with zipfile.ZipFile("/tmp/latest.zip") as zip:
-    print("Extracting files to %s"%os.environ["INPUT_OUTPUT_DIR"])
-    zip.extractall(os.environ["INPUT_OUTPUT_DIR"])
-
-print("Files extracted, dir contents are:")
-pprint(os.listdir(os.environ["INPUT_OUTPUT_DIR"]))
+    print("Files extracted, dir contents are:")
+    pprint(os.listdir(os.environ["INPUT_OUTPUT_DIR"]))
+except zipfile.BadZipFile as bzf:
+    print("Could not download the artifact as zip message was: %s"%resp.json)
+    pprint(resp.text)
+    with open("/tmp/latest.zip") as l:
+        pprint(l.read())
