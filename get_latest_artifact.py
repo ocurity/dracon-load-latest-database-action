62a720e2c86531483c0f8e268f979eb7cb69ead7
spyros <morfeas3000@gmail.com>
2022-04-27 18:49:36 +0100

    wip

--- get_latest_artifact.py
+++ get_latest_artifact.py
@@ -20,10 +20,8 @@ for art in artifacts:
     if d > latest:
         latest = d
         artifact_url = art["archive_download_url"]
-pprint(f"using ACTIONS_RUNTIME_TOKEN ")
-pprint([f for f in os.environ["ACTIONS_RUNTIME_TOKEN"]])
-pprint([f for f in os.environ["INPUT_GH_ACCESS_TOKEN"]])
-pprint([f for f in os.environ.items()])
+
+pprint( os.environ["ACTIONS_RUNTIME_TOKEN"] == os.environ["INPUT_GH_ACCESS_TOKEN"])
 token = os.environ["ACTIONS_RUNTIME_TOKEN"] # os.environ["INPUT_GH_ACCESS_TOKEN"]
 resp = requests.get(artifact_url, stream=True,headers={"Authorization" :"token %s"%token})
 if resp.status_code != 200:
